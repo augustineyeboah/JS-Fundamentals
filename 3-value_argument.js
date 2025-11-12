fb08e6e1ceac4ef9f82b9c972f1764bb898d1456
Augustine Yeboah <arealaugustine@gmail.com>
2025-11-12 22:12:48 +0000

    Added a variable and printed it for testing.

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,5 +1,7 @@
 const nodeArgs = process.argv.slice(2);
 
-  if (nodeArgs[0] === "") {
-    console.log("No argument");
-  }
+if (!nodeArgs[0]) {
+  console.log("No argument");
+} else {
+  console.log(nodeArgs[0]);
+}
