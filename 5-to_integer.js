2e0a5cdc3ad2e321e86f6bb82e43a64d4271b24b
Augustine Yeboah <arealaugustine@gmail.com>
2025-11-12 22:37:43 +0000

    Added a variable and printed it for testing.

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,9 @@
+
 const nodeArgs = process.argv.slice(2);
 const num = Math.floor(Number(nodeArgs[0]));
 
 if (Number.isInteger(num)) {
-  console.log(My number: ${num});
+  console.log(“My number:”, num);
 } else {
   console.log("Not a number");
 }
