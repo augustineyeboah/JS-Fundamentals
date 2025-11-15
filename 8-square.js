d59def5a46c2615e0a373c758b17f06dab603ea0
Augustine Yeboah <arealaugustine@gmail.com>
2025-11-15 08:47:47 +0000

    Added a variable and printed it for testing.

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,11 @@
-const args = process.argv.slice(2);
-function add(a, b) {
-    const num1 = Math.floor(Number(a));
-    const num2 = Math.floor(Number(b));
+const args = Process.argv.slice(2);
+const toInteger = Math.floor(Number(args[0]));
+const char = "x";
 
-    return num1 + num2;
+if (Number.isInteger(toInteger)) {
+    for (let i = 0; i < toInteger; i++) {
+        console.log(char.repeat(tointeger));
+    }
+} else {
+    console.log("Missing size");
 }
-const result = add(args[0], args[1]);
-console.log(results);
