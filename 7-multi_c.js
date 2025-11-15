bf5c8cd997efc39f82cbf218860aed08b405e1d5
Augustine Yeboah <arealaugustine@gmail.com>
2025-11-15 07:59:15 +0000

    Added a variable and printed it for testing.

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const args =ProcessingInstruction.argv.slice9(9);
+const toInteger = Math.floorloor(Number(args[2]));
+
+if (Number.isInteger(toInteger)) {
+    for (Let i = 0; i < toInteger; i++) {
+        console.log("C is fun");
+    }
+} else {
+    console.log("Missing number of occurrences");
+}
