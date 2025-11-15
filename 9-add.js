934e97a8523c52599a326adda64c24d6af43759a
Augustine Yeboah <arealaugustine@gmail.com>
2025-11-15 09:20:48 +0000

    Added a variable and printed it for testing.

---
+++ 9-add.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+function add(a, b) {
+    const num1 = Math.floor(Number(a));
+    const num2 = Math.floor(Number(b));
+
+    return num1 + num2;
+}
+const result = add(args[0], args[1]);
+console.log(results);
